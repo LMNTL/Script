4b4efc3b4c802498924a3fd463e3272f7d1015ea
OliveAwesomesauz <OliveAwesomesauz@gmail.com>
2015-04-27 13:32:14 -0500

    Small refactoring in NICSpec.js

--- spec/NICSpec.js
+++ spec/NICSpec.js
@@ -1,22 +1,23 @@
 describe("NIC", function() {
-  var space
+  var space;
   var device;
-  var script;
 
   beforeEach(function() {
     space = new Space();
-    device = new Device();
-    script = new Script();
-    space.devices.push(device);
+    device = [
+      new Device(),
+      new Device(),
+      new Device()
+    ];
+    space.devices = device;
   });
 
   it("should connect to other devices", function() {
-    var device2 = new Device();
-    device.nic.connectTo(device2);
-    expect(device.nic.isConnected(device2)).toEqual(true);
-    expect(device2.nic.isConnected(device)).toEqual(true);
-    device.nic.disconnectFrom(device2);
-    expect(device.nic.isConnected(device2)).toEqual(false);
-    expect(device2.nic.isConnected(device)).toEqual(false);
+    device[0].nic.connectTo(device[1]);
+    expect(device[0].nic.isConnected(device[1])).toEqual(true);
+    expect(device[1].nic.isConnected(device[0])).toEqual(true);
+    device[0].nic.disconnectFrom(device[1]);
+    expect(device[0].nic.isConnected(device[1])).toEqual(false);
+    expect(device[1].nic.isConnected(device[0])).toEqual(false);
   });
 });
