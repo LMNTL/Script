2e4695c3fcf56c7770c6b24f56bba612863d42eb
OliveAwesomesauz <OliveAwesomesauz@gmail.com>
2015-05-02 13:46:54 -0500

    All tests green

--- spec/CPUSpec.js
+++ spec/CPUSpec.js
@@ -10,18 +10,6 @@ describe("CPU", function() {
     space.devices.push(device);
   });
 
-  it("should run scripts", function() {
-    script.runtime = 2;
-    space.step();
-    expect(device.cpu.progress).toEqual(0);
-    device.cpu.enqueue(script);
-    space.step();
-    expect(device.cpu.progress).toEqual(1);
-    space.step();
-    space.step();
-    expect(device.cpu.progress).toEqual(0);
-  });
-
   it("should queue scripts", function() {
     script.runtime = 2;
     var script2 = Script.get("displayText");
@@ -35,27 +23,18 @@ describe("CPU", function() {
   });
 
   it("should complete scripts", function() {
-    var script = new Script({
-      runtime: 1,
-      complete: function(device) {
-        device.gpu.display('Script done!');
-      }
-    });
-    device.cpu.enqueue(script);
+    var script = Script.get('displayText');
+    device.cpu.enqueue(script.instance(['Script done!']));
     space.step();
     expect(device.gpu.displaying).toEqual('Script done!');
   });
 
   it("should have a processing speed", function() {
     device.cpu.speed = 2;
-    script.runtime = 2;
-    var script2 = new Script({
-      runtime: 1
-    });
-    device.cpu.enqueue(script);
-    device.cpu.enqueue(script2);
+    device.cpu.enqueue(script.instance(['meow']));
+    device.cpu.enqueue(script.instance(['meow']));
     space.step();
-    expect(device.cpu.activeScript).toEqual(script2);
+    expect(device.cpu.queue.length).toEqual(0);
   });
 
   it("should have a memory capacity", function() {
